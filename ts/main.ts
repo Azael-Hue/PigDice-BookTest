33b0ffe749f0170de2fdee06c81423575761a4e0
Azael H. <166276745+Azael-Hue@users.noreply.github.com>
2024-06-10 09:15:17 -0700

    added function to typescript to make a new game, roll die, change player and accumulate points and added a link to JQuery on the HTML

--- ts/main.ts
+++ ts/main.ts
@@ -12,3 +12,71 @@ const getRandomNumber = max => {
     }
     return rand;
 }
+
+// This function changes the player's when the turn is over
+const changePlayer = () => {
+    if ( $("#current").text() == $("#player1").val() ) {
+        $("#current").text( $("#player2").val() );
+    }
+    else {
+        $("#current").text( $("#player1").val() );
+    }
+    $("#die").val("0");
+    $("#total").val("0");
+    $("#roll").focus();
+};
+
+$( document ).ready( () => {
+    // this function is called when the new game button is clicked
+    $("#new_game").click( () => {
+        $("#score1").val("0");
+        $("#score2").val("0");
+
+        if ( $("#player1").val() == "" || $("#player2").val() == "" ) {
+            alert("Please enter two player names.");
+        }
+        else {
+            $("#turn").removeClass("hide");
+            changePlayer();
+        }
+    })
+
+    // this function is called when the roll button is clicked
+    $("#roll").click( () => {
+        let total = parseInt( $("#total").val() );
+        const die = getRandomNumber(6);
+
+        if (die == 1) {
+            total = 0;
+            changePlayer();
+        }
+        else {
+            total = total + die;
+        }
+        $("#die").val(die);
+        $("#total").val(total);
+    })
+
+    // this function is called when the hold button is clicked
+    $("#hold").click( () => {
+        let score = 0;
+        const total = parseInt( $("#total").val() );
+
+        if ( $("#current").text() == $("#player1").val() ) {
+            score = $("#score1");
+        }
+        else {
+            score = $("#score2");
+        }
+
+        score.val( parseInt( score.val() ) + total );
+        if (score.val() >= winningTotal) {
+            alert( $("#current").text() + " WINS!" );
+        }
+        else {
+            changePlayer();
+        }
+    })
+    $("#winning_total").text(winningTotal);
+    $("#player1").focus();
+});
